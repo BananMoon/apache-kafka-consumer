8db0dfa1d7e476a8563a8c30ca58f84484d00f8d
BananMoon <younji1115@gmail.com>
2024-07-29 20:03:38 +0900

    feat: init project

---
+++ src/test/java/com/moonz/study/ApacheKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonz.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
